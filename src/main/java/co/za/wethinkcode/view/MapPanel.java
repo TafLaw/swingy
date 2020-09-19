a6fe9ec743fbb5ad196b710588ee35103308fb17
TafLaw <muzerenganit@gmail.com>
2020-09-19 09:58:01 +0200

    removed comments

--- src/main/java/co/za/wethinkcode/view/MapPanel.java
+++ src/main/java/co/za/wethinkcode/view/MapPanel.java
@@ -67,7 +67,7 @@ public class MapPanel extends JPanel {
     }
 
     private void drawGrid(Graphics2D g2){
-//        g2.setColor(Color.WHITE);
+        g2.setColor(Color.black);
         for (int i = 0; i < rows + 1; i++) {
             g2.drawLine(originX, originY + i * cellSide, originX + cols * cellSide, originY + i * cellSide);
         }
